ab77ff0780f1075d1b3f62e3bbe31ef766ffb555
bairuiwang <bairuiwang@tencent.com>
2019-10-16 11:54:46 +0800

    delete all

--- debug_model.sh
+++
@@ -1,3 +0,0 @@
-#CUDA_VISIBLE_DEVICES=7 python eval.py --model ../results/msrvtt_Resrgbfc_i3dflowfc_NO14mask_globalpos_dgate/model-best.pth --infos_path ../results/msrvtt_Resrgbfc_i3dflowfc_NO14mask_globalpos_dgate/infos_msrvtt_Resrgbfc_i3dflowfc_14mask_globalpos-best.pkl --beam_size 1
-
-CUDA_VISIBLE_DEVICES=7 python eval.py --model ../results/msrvtt_Resrgbfc_i3dflowfc_NO14mask_globalpos_dgate_RL/model-best.pth --infos_path ../results/msrvtt_Resrgbfc_i3dflowfc_NO14mask_globalpos_dgate_RL/infos_msrvtt_Resrgbfc_i3dflowfc_14mask_globalpos-best.pkl --beam_size 1
